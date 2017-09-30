6d7a9b6c65cd948eba4aee5f51e8b390cd0db9b0
unknown <yinglarp@gmail.com>
2017-09-30 13:25:50 +0800

    data science works

---
+++ notes_data_science.py
@@ -0,0 +1,10 @@
+classification (got labels used, supervised learning) vs clustering (no labels, unsupervised learning)
+
+KNN - classification - calculate the distance between data points and make a prediction
+logistics regression -assume data linear relationship, Logistic Regression is usually used to predict result according to the probability.
+decision tree - can handle non-linear relationship, prone to overfitting, To use Decision Tree, you should transform the continuous variable into categorical.
+URL: https://datascience.stackexchange.com/questions/6048/decision-tree-or-logistic-regression
+
+Precision: High Precision = few false positive
+Recall: High Recall = Few false negative
+Typically, we want higer recall than precision. To improve both measures, either increase the data sample size to run more iterations or fine tune the model variables
